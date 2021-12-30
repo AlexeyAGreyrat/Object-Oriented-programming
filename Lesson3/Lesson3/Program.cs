199493c11ae50e0c2b3e1bbb55657883b21d8d49
Алексей Афанасьев <75749232+AlexeyAGreyrat@users.noreply.github.com>
2021-12-30 19:00:55 +0300

    Lesson3

---
+++ Lesson3/Lesson3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace Lesson3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            BankAccount bankA = new BankAccount(100);         
+            BankAccount bankB = new BankAccount(200);
+            bankA.Transfer(bankB, 100000);
+            bankB.Transfer(bankA, 1000);
+            Console.WriteLine($"\n{Palindromes.Palindrom("Program")}");
+            string textFromFile;
+            using (FileStream fstream = File.OpenRead(@"..\..\..\Text.txt"))
+            {
+                byte[] array = new byte[fstream.Length];
+                fstream.Read(array, 0, array.Length);
+                textFromFile = System.Text.Encoding.Default.GetString(array);
+                Console.WriteLine($"Текст из файла: {textFromFile}");
+            }
+            string s = textFromFile;
+            Console.WriteLine();
+            Search.SearchMail(ref s);
+            Console.WriteLine(s);
+
+        }
+    }
+}
---
+++ Lesson3/Lesson3/Search.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Lesson3
+{
+    class Search
+    {
+        static public void SearchMail(ref string s)
+        {
+            var str = s.Split('&','\n');
+            string pow = null;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (string.IsNullOrWhiteSpace(str[i]))
+                {
+                    break;
+                }
+                else
+                {
+                    str[i] = str[i].Trim();
+                    var ser = Regex.IsMatch(str[i],
+                           @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+                           RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+                    if(ser == true)
+                    {
+                        pow = pow + str[i] + "\n";
+                    }
+                }                
+            }
+            s = pow;
+        }
+    }
+}
