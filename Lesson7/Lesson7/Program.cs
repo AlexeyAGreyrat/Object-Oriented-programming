980fd58edce76dd042e2a3945e649753b500757e
Алексей Афанасьев <75749232+AlexeyAGreyrat@users.noreply.github.com>
2022-01-24 18:47:42 +0300

    Lesson7

---
+++ Lesson7/Lesson7/Program.cs
@@ -0,0 +1,12 @@
+﻿using Lesson7;
+
+ACoder coder = new ACoder();
+BCoder coderB = new BCoder();
+
+
+Console.WriteLine(coder.Encode("абвгдежзийклмнопрстуфхцчшщьыъэюя"));
+Console.WriteLine(coder.Decode("бвгдежзийклмнопрстуфхцчшщьыъэюяа"));
+Console.WriteLine(coderB.Encode("абвгдежзийклмнопрстуфхцчшщьыъэюя"));
+Console.WriteLine(coderB.Decode("яюэъыьщшчцхфутсрпонмлкйизжедгвба"));
+
+
