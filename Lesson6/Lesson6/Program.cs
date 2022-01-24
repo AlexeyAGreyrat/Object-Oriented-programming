90e8d9d18ebb76c800bfd7eca43d146875babac5
Алексей Афанасьев <75749232+AlexeyAGreyrat@users.noreply.github.com>
2022-01-24 18:46:55 +0300

    Lesson6

---
+++ Lesson6/Lesson6/Program.cs
@@ -0,0 +1,14 @@
+﻿using Lesson6;
+
+BankAccount bank = new BankAccount(1,200000);
+BankAccount bank2 = new BankAccount(2,100000);
+
+Console.WriteLine($"{bank} \n{bank2}");
+Console.WriteLine($"{bank.GetHashCode()}");
+Console.WriteLine($"{bank2.GetHashCode()}");
+Console.WriteLine(bank.Equals(bank));
+Console.WriteLine(bank.Equals(bank2));
+Console.WriteLine(bank != bank2);
+Console.WriteLine(bank == bank2);
+Console.WriteLine(bank == bank);
+
